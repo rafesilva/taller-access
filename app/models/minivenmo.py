64fdd91ed608ba41ce093051025cb5d84b17897b
Rafael Ferreira da Silva <rafesilva@gmail.com>
2025-03-17 15:58:05 -0300

    First commit

---
+++ app/models/minivenmo.py
@@ -0,0 +1,23 @@
+class MiniVenmo:
+
+	def __init__(self):
+		self.users = {}
+
+
+	def create_user(self, name, initial_balance=0.0, credit_card:None):
+		if name self.users:
+			return "User already exists"
+		user = User(name, initial_balance)
+		self.users[name] = user
+		return user
+
+
+	def render_feed(self):
+		feed = []
+		for user in self.users.values():
+			feed.extend(user.retrieve_activity())
+
+		return sorted(feed, reverse=True)
+
+
+
